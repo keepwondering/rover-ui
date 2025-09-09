1efe395cc72d06d10d6b2f4ee4367bc68d06873b
Martin Rafael Gonzalez <tin@devtin.io>
2025-09-09 11:03:15 -0500

    refactor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/test-utils',
-    '@nuxt/fonts',
-    '@nuxt/content'
-  ]
-})
+  nitro: {
+    output: {
+      publicDir: "docker/dist", // static site goes here -> docker/dist
+    },
+  },
+
+  modules: ["@nuxt/eslint", "@nuxt/test-utils", "@nuxt/fonts", "@nuxt/content"],
+});
